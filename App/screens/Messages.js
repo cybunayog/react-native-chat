d5f5b45f39b15a0e1f232c8cd7e3616c3a705091
Cy Bunayog <cybunayog@gmail.com>
2020-10-26 19:12:12 -0700

    Get Real Time Messages

--- App/screens/Messages.js
+++ App/screens/Messages.js
@@ -4,31 +4,55 @@ import firebase from 'react-native-firebase';
 
 export default class Messages extends React.Component {
   state = {
-    messages: [
-      {
-        _id: 0,
-        text: 'thread created',
-        createdAt: new Date().getTime(),
-        system: true,
-      },
-      {
-        _id: 1,
-        text: 'hello!',
-        createdAt: new Date().getTime(),
-        user: {
-          _id: 2,
-          name: 'Demo',
-        },
-      },
-    ],
+    messages: [],
   };
+
+  componentDidMount() {
+    const thread = this.props.navigation.getParam('thread');
+    this.removeMessagesListener = firebase
+      .firestore()
+      .collection('MESSAGE_THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(querySnapshot => {
+        const messages = querySnapshot.docs.map(doc => {
+          const firebaseData = doc.data();
+
+          // System message
+          const data = {
+            _id: doc.id,
+            text: '',
+            createdAt: new Date().getTime(),
+            ...firebaseData,
+          };
+
+          // If message didn't come from the system
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.displayName,
+            };
+          }
+
+          return data;
+        });
+
+        this.setState({messages});
+      });
+  }
+
+  componentWillUnmount() {
+    if (this.removeMessagesListener) {
+      this.removeMessagesListener();
+    }
+  }
 
   handleSend = async messages => {
     const text = messages[0].text;
     // Locates the thread being used to send a message.
     const thread = this.props.navigation.getParam('thread');
     const user = firebase.auth().currentUser.toJSON();
-    // console.log(thread, text);
 
     await firebase
       .firestore()
@@ -60,11 +84,13 @@ export default class Messages extends React.Component {
   };
 
   render() {
+    const user = firebase.auth().currentUser.toJSON();
+
     return (
       <GiftedChat
         messages={this.state.messages}
         onSend={this.handleSend}
-        user={{_id: 1}}
+        user={{_id: user.uid}}
       />
     );
   }
